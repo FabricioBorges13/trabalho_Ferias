c0a2f9a0845aa3f431ed3cf598691168a1a52bad
Fabricio <fabricio@uniplaclages.edu.br>
2018-08-13 16:26:21 -0300

    iniciado tasks

---
+++ SolidOpsTrabalho.Aplicacao/Features/Vendas/VendaTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOpsTrabalho.Aplicacao.Features.Vendas
+{
+    public class VendaTask
+    {
+        public VendaTask()
+        {
+            var vendaTask = Task.Run(() => LerArquivos());
+        }
+
+        private void LerArquivos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
