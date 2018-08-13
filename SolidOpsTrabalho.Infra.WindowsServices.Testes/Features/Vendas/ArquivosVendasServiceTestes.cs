8252ef585ba021e51165d2ce9b633a03f80db5db
Elison Coelho <elison_nc@hotmail.com>
2018-08-13 17:46:56 -0300

    iniciado o servico

---
+++ SolidOpsTrabalho.Infra.WindowsServices.Testes/Features/Vendas/ArquivosVendasServiceTestes.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SolidOpsTrabalho.Infra.WindowsServices.Features.Vendas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOpsTrabalho.Infra.WindowsServices.Testes.Features.Vendas
+{
+    [TestFixture]
+    class ArquivosVendasServiceTestes
+    {
+
+        [Test]
+        public void DeveAbrirERodarOServico()
+        {
+            //var servico = new ArquivosVendasService();
+            //servico.Iniciar();
+            var ArquivosVendasService = new ArquivosVendasService();
+
+        }
+
+    }
+}
