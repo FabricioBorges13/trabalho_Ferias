06b3f80f2f19b9f91e804e86741449575d9c1ace
Fabricio <fabricio@uniplaclages.edu.br>
2018-08-14 17:02:52 -0300

    Criado Service, alterado task

---
+++ SolidOpsTrabalho.Aplicacao/Features/Vendas/VendaService.cs
@@ -0,0 +1,29 @@
+﻿using SolidOpsTrabalho.Infra.CSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOpsTrabalho.Aplicacao.Features.Vendas
+{
+    public class VendaService : IVendaService
+    {
+       
+        public void Atualizar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finalizar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inicializar()
+        {
+            throw new NotImplementedException();
+        }
+                
+    }
+}
