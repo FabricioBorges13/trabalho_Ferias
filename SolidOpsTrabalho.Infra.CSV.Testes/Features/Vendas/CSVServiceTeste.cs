cacfa6b0ddc8d9d97945334c06d544fdd14ad7c4
vinicius <vinicius@DESKTOP-1OF727D>
2018-08-15 03:35:11 -0300

    montando testes

---
+++ SolidOpsTrabalho.Infra.CSV.Testes/Features/Vendas/CSVServiceTeste.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOpsTrabalho.Infra.CSV.Testes.Features.Vendas
+{
+    [TestFixture]
+    public class CSVServiceTeste
+    {
+
+
+        string caminhoCsvTeste;
+
+
+        public void gerarArquivo() {
+            
+        }
+        
+    }
+}
