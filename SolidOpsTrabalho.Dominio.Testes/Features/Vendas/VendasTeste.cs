2c78c55aa57c5eb4c5aec896a6d26da569fb95b5
johnson sadao <Johnson9697@hotmail.com>
2018-08-17 18:05:10 -0300

    Ajustes do FakeDb

--- SolidOpsTrabalho.Dominio.Testes/Features/Vendas/VendasTeste.cs
+++ SolidOpsTrabalho.Dominio.Testes/Features/Vendas/VendasTeste.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SolidOpsTrabalho.Comum.Testes.Features;
+using SolidOpsTrabalho.Dominio.Features.Vendas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,17 @@ using System.Threading.Tasks;
 
 namespace SolidOpsTrabalho.Dominio.Testes.Features.Vendas
 {
+    [TestFixture]
     public class VendasTeste
     {
+        [Test]
+        public void Venda_Preco_Total()
+        {
+            double PrecoEsperado = 1;
+            Venda venda = ObjectMother.Venda;
+
+            venda.PrecoTotal.Should().Be(PrecoEsperado);
+        }
+
     }
 }
