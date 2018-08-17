fe2e1ddd86fa8812b287eb660138eade27bb78d9
johnson sadao <Johnson9697@hotmail.com>
2018-08-17 16:52:59 -0300

    Criação de classes

---
+++ SolidOpsTrabalho.Dominio.Testes/Features/Vendas/VendasTeste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidOpsTrabalho.Dominio.Testes.Features.Vendas
+{
+    public class VendasTeste
+    {
+    }
+}
