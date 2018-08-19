eb00279af9c37deb4c8c169b11c743091a1a6e95
Fabricio <fabricio@uniplaclages.edu.br>
2018-08-18 23:27:08 -0300

    Criado validação de 3 vezes venda invalida

--- SolidOpsTrabalho.Aplicacao/Features/Vendas/VendaTask.cs
+++ SolidOpsTrabalho.Aplicacao/Features/Vendas/VendaTask.cs
@@ -66,24 +66,18 @@ namespace SolidOpsTrabalho.Aplicacao.Features.Vendas
 
         private void ValidarVenda(Venda venda)
         {
-            bool valida = false;
-            var x = venda.Validar();
+
+            bool x = venda.Validar();
             int i = 0;
-            while (!valida)
+             while(!x  && i < 3)
             {
-                if (venda == null || !x || i > 2)
-                {
-                    TaskMoverInvalida(venda);
-                    
-                }
-                   
-                else
-                {
-                    TaskMoverValida(venda);
-                    break;
-                }
-                    
+                x = venda.Validar();
+                i++;
             }
+            if (!x)
+                TaskMoverInvalida(venda);
+            else
+                TaskMoverValida(venda);
                       
         }
 
